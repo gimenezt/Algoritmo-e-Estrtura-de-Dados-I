f381dfc0551e1bcc527bae35013c9446b1192d3c
Lilian G <97319339+gimenezt@users.noreply.github.com>
2022-05-20 11:52:56 -0300

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -3,12 +3,12 @@
 int main(){
     setlocale (LC_ALL, "Portuguese");
     
-    int cx=150, i, metodo[5], ccheque=0, ccc=0, cdinheiro=0;
+    int cx=150, i, metodo[150], ccheque=0, ccc=0, cdinheiro=0;
     float somacheque, somacc, somadin, somatotal;
     
     printf("ADMINISTRAÇÃO DE PAGAMENTOS - DANCETERIA\n\n");
     
-    for(i=0;i<5;i++) {
+    for(i=0;i<150;i++) {
         printf("\nDigite o número de acordo com o método de pagamento do %dº cliente: \n1 - CHEQUE\n2 - CARTÃO DE CRÉDITO\n3 - DINHEIRO(CÉDULAS)\n\n", i+1);
         scanf("%d", &metodo[i]);
             if(metodo[i]==1) 
@@ -20,7 +20,7 @@ int main(){
                 
     float cheque[ccheque], cc[ccc], dinheiro[cdinheiro];
     
-    for(i=0;i<5;i++) {
+    for(i=0;i<150;i++) {
     //cheque
     if(metodo[i]==1) {
             printf("\n\nDigite o valor pago pelo %dº cliente: R$ ", i+1);
@@ -52,5 +52,4 @@ int main(){
     printf("\n\nHouveram %d pagamento(s) em cartão de crédito.\nO total de dinheiro entrado a partir desse método de pagamento foi R$ %.2f", ccc, somacc);
     
     printf("\n\nHouveram %d pagamento(s) em dinheiro.\nO total de dinheiro entrado a partir desse método de pagamento foi R$ %.2f", cdinheiro, somadin);
-    
 }
